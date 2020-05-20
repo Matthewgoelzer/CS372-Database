e1180a0f77d7bfe0aa86ca5e376b6b7714b5a432
Matthew Goelzer <mhankg@gmail.com>
2020-05-20 15:49:38 -0600

    updated init and DMQ to most recent version

--- backend/DMQ.sql
+++ backend/DMQ.sql
@@ -13,6 +13,9 @@ INSERT INTO volunteer_Profiles (volunteer_Name, volunteer_Email, volunteer_DOB, 
 -- add a new organization
 INSERT INTO nonprofit_Organizations (organization_Name) VALUES (:user_Entered_Name);
 
+-- add new job applicant
+INSERT INTO `job_Applicants` (`volunteer_ID`, `job_ID`, `approved`) VALUES ((SELECT volunteer_ID FROM volunteer_Profiles WHERE volunteer_Name = ?), ?, "0")
+
 -- update an volunteer's application
 UPDATE job_Applicants SET approved = :checkbox_From_User_To_Indicate_Approved WHERE volunteer_ID= :volunteer_ID_from_the_update_form
 
--- backend/init.sql
+++ backend/init.sql
@@ -1,28 +1,24 @@
 /* table for nonprofit organizations */
 CREATE TABLE nonprofit_Organizations (
     organization_Name varchar(255) NOT NULL UNIQUE,
-    organization_ID int NOT NULL UNIQUE,
-    job_ID int,
+    organization_ID int(11) AUTO_INCREMENT,
     PRIMARY KEY (organization_ID)
 ) ENGINE=InnoDB;
 
 /* table for job postings */
 CREATE TABLE job_Postings (
     job_Title varchar(255) NOT NULL UNIQUE,
-    job_ID int NOT NULL UNIQUE,
+    job_ID int(11) AUTO_INCREMENT,
     organization_ID int,
     PRIMARY KEY (job_ID)
 ) ENGINE=InnoDB;
-
-ALTER TABLE nonprofit_Organizations
-ADD CONSTRAINT fk1 FOREIGN KEY (job_ID) REFERENCES job_Postings(job_ID);
 
 ALTER TABLE job_Postings
 ADD CONSTRAINT fk2 FOREIGN KEY (organization_ID) REFERENCES nonprofit_Organizations(organization_ID);
 
 /*table for volunteer profiles */
 CREATE TABLE volunteer_Profiles (
-    volunteer_ID int NOT NULL UNIQUE,
+    volunteer_ID int(11) AUTO_INCREMENT,
     volunteer_Name varchar(255) NOT NULL UNIQUE,
     volunteer_Email varchar(255) NOT NULL UNIQUE,
     volunteer_DOB date NOT NULL,
@@ -33,22 +29,19 @@ CREATE TABLE volunteer_Profiles (
 /* table for job applicants */
 CREATE TABLE job_Applicants (
     volunteer_ID int,
-    organization_ID int,
     job_ID int,
     approved BOOLEAN NOT NULL,
     FOREIGN KEY (volunteer_ID) REFERENCES volunteer_Profiles(volunteer_ID),
-    FOREIGN KEY (organization_ID) REFERENCES nonprofit_Organizations(organization_ID),
-    FOREIGN KEY (job_ID) REFERENCES job_Postings(job_ID)
+    FOREIGN KEY (job_ID) REFERENCES job_Postings(job_ID),
+    PRIMARY KEY(volunteer_ID, job_ID)
 ) ENGINE=InnoDB;
 
 /* table for volunteer histories */
 CREATE TABLE volunteer_Histories (
     volunteer_ID int,
     job_ID int,
-    organization_ID int,
     FOREIGN KEY (volunteer_ID) REFERENCES volunteer_Profiles(volunteer_ID),
-    FOREIGN KEY (job_ID) REFERENCES job_Postings(job_ID),
-    FOREIGN KEY (organization_ID) REFERENCES nonprofit_Organizations(organization_ID)
+    FOREIGN KEY (job_ID) REFERENCES job_Postings(job_ID)
 ) ENGINE=InnoDB;
 
 INSERT INTO nonprofit_Organizations (organization_Name) VALUES ("nonprofit-organization-1");
